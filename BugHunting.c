0e5bd1c3ea1f1aa8731c5540582f2bc553e36c2f
onurb26 <120804948+onurb26@users.noreply.github.com>
2023-02-03 18:06:29 +0100

    Add files via upload

--- BugHunting.c
+++ BugHunting.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <ctype.h>
 
@@ -6,16 +5,17 @@ void menu();
 void moltiplica();
 void dividi();
 void ins_string();
-
-
+ 
+//Corpo principale del programma
 int main()
 {
-	char scelta = {'\0'};
-	menu();
-	scanf (" %c",&scelta);
-while(toupper(scelta)<'A' || toupper(scelta)>'C'){
-printf("Input Errato! Riprova ->");
-                scanf(" %c",&scelta);
+	char scelta;
+do{                                                //il ciclo  do fa si che l'utente possa continuare ad usare il    
+	menu();                                    //programma finchè non decide di terminare.
+	scanf(" %c",&scelta);
+while(toupper(scelta)<'A' || toupper(scelta)>'D'){ //toupper è una funzione che rende maiuscolo  un carattere char 
+printf("Input Errato! Riprova ->");                //in questo caso viene utilizzato per far si che se anche l' utente 
+                scanf(" %c",&scelta);              //inserisce una lettera minuscola, viene convertita
         }
 
 	switch (toupper(scelta))
@@ -29,8 +29,10 @@ printf("Input Errato! Riprova ->");
 		case 'C':
                 ins_string();
                 break;
+                case 'D':
+                break;
 	}
-
+}while(toupper(scelta)!='D');
 return 0;
 
 }
@@ -40,7 +42,7 @@ void menu()
 {
 	printf ("Benvenuto, sono un assitente digitale, posso aiutarti a sbrigare alcuni compiti\n");
 	printf ("Come posso aiutarti?\n");
-	printf ("A >> Moltiplicare due numeri\nB >> Dividere due numeri\nC >> Inserire una stringa\n");
+	printf ("A >> Moltiplicare due numeri\nB >> Dividere due numeri\nC >> Inserire una stringa\nD >> Esci dal programma\n");
 
 }
 
@@ -48,14 +50,14 @@ void menu()
 void moltiplica()
 {
         int  a=0,b=0,c1=0,c2=0;
-	printf ("Inserisci i due numeri da moltiplicare:");
+	printf ("Inserisci i due numeri da moltiplicare:");    
         c1=scanf ("%d",&a);
         c2=scanf ("%d",&b);
-        if(c1==0 || c2==0)
+        if(c1==0 || c2==0)                   //controlla che l'untente solo numeri 
            printf("Errore input!"); 
         else{
            int prodotto = a * b;
-           printf ("Il prodotto tra %d e %d e': %d", a,b,prodotto);
+           printf ("Il prodotto tra %d e %d e': %d\n", a,b,prodotto);
         }
 }
 
@@ -71,12 +73,12 @@ void dividi()
          }else{
 	  printf("Inserisci il denumeratore:");
           c2=scanf ("%d", &b);
-        if(c2==0 || b<1){
+        if(c2==0 || b<1){                       
            printf("Errore input!");
           return;
         }else{
         float divisione=(float)a /(float) b;
-        printf ("La divisione tra %d e %d e': %f", a,b,divisione);
+        printf ("La divisione tra %d e %d e': %f\n", a,b,divisione);
         }
        }
 }
@@ -89,7 +91,7 @@ void ins_string()
 {
 	char stringa[10];
         printf("Inserisci la stringa (max 9 caratteri): ");
-        scanf(" %9[^\n]",&stringa);
+        scanf(" %9[^\n]",&stringa);      //Inserisce nella variabile stringa  fino a 9 caratteri, inclusi spazi 
         printf("Stringa Inserita: %s",stringa);
 
 }
